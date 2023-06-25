5f61efa7764549f601dded93aba21eb245db3063
Liam Cammpbell <liam22campbell@gmail.com>
2023-06-25 16:33:00 -0700

    Add movement

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,11 @@
-import Sprite from './biz-func.js';
+import Sprite from './sprite.js';
 // import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../css/styles.css';
 
 
-const canvas = document.querySelector('canvas');
+export const canvas = document.querySelector('canvas');
 export const c = canvas.getContext('2d');
 //canvas apppearance
 canvas.width = 1024;
@@ -15,31 +15,142 @@ canvas.height = 524;
 c.fillRect(0,0,canvas.width, canvas.height);
 
 
-// animation loop 
-function animate() {
-  window.requestAnimationFrame(animate)
- 
-}
-
-animate()
-
 // Player and Enemy
 // could add width and height
 const player = new Sprite({
-  x: 0,
-  y: 0, 
+  position : {
+    x: 0,
+    y: 0 },
+  color: 'red',
+  velocity: {
+    x: 0,
+    y: 0
+  },
+  speed: 5,
+  jumpHeight: 20,
   
-}, 'red');
+});
 
 
-player.draw();
+
 
 
 const enemy = new Sprite({
-  x: 950,
-  y: 0,
-},
- 'blue');
-enemy.draw();
+  position : {
+    x: 950,
+    y: 0 },
+  color: 'blue',
+  velocity: {
+    x: 0,
+    y: 0
+  },
+  speed: 4.5,
+  jumpHeight: 22,
+});
+
+
+const keys = {
+  a: {
+    pressed: false
+  },
+  d: {
+    pressed: false
+  },
+  arrowLeft: {
+    pressed: false
+  },
+  arrowRight: {
+    pressed: false
+  }
+};
+
+
+// animation loop 
+function animate() {
+  window.requestAnimationFrame(animate);
+  c.fillStyle = 'black';
+  c.fillRect(0,0, canvas.width, canvas.height);
+  player.update();
+  enemy.update();
+  
+  if(keys.a.pressed === true && player.lastKey === 'a'){
+    player.velocity.x = - player.speed;
+  } else if ( keys.d.pressed === true && player.lastKey === 'd'){
+    player.velocity.x = player.speed;
+  } else {
+    player.velocity.x = 0;
+  }
+  
+  if(keys.arrowLeft.pressed === true && enemy.lastKey === 'aL'){
+    enemy.velocity.x = -enemy.speed;
+  }else if (keys.arrowRight.pressed === true && enemy.lastKey === 'aR'){
+    enemy.velocity.x = enemy.speed;
+  } else enemy.velocity.x = 0;
+  
+}
+
+animate();
+
+
+window.addEventListener('keydown', (event) =>{
+  console.log(event.key);
+  switch (event.key) {
+  case 'd':
+    keys.d.pressed = true;
+    player.lastKey = 'd';
+    break;
+  case 'a':
+    keys.a.pressed = true;
+    player.lastKey  = 'a';
+    break;
+  case 'w':
+    player.jump();
+    break;
+  case 's': 
+    player.duck();
+    break;
+  case 'ArrowRight':
+    keys.arrowRight.pressed = true;
+    enemy.lastKey = 'aR';
+    break;
+  case 'ArrowLeft': 
+    keys.arrowLeft.pressed = true;
+    enemy.lastKey = 'aL';
+    break;
+  case 'ArrowUp': 
+    enemy.jump();
+    break;
+  case 'ArrowDown':
+    enemy.duck();
+    break;
+  } 
+});
+
+window.addEventListener('keyup', (event) =>{
+  console.log(event.key);
+  switch (event.key) {
+  case 'd':
+    keys.d.pressed = false;
+    break;
+  case 'a':
+    keys.a.pressed = false;
+    break;
+  case 's':
+    player.stand();
+    break;
+  case 'ArrowLeft':
+    keys.arrowLeft.pressed = false;
+    break;
+  case 'ArrowRight':
+    keys.arrowRight.pressed = false;
+    break;
+  case 'ArrowDown': 
+    enemy.stand();
+    break;
+  }
+});
+
+
+
 console.log(player);
 
