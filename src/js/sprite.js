5ae6048366f633e9e143f0f3578eee71f06db3b9
Liam Cammpbell <liam22campbell@gmail.com>
2023-06-25 15:30:25 -0700

    Add fall mechanics

--- src/js/sprite.js
+++ src/js/sprite.js
@@ -1,21 +1,33 @@
 // import $ from 'jquery';
-import { c }from './index.js';
+import { c, canvas }from './index.js';
+
+const gravity = .2;
 
 class Sprite {
   constructor({position, color, velocity}) {
     this.position = position;
     this.color = color;
     this.velocity = velocity;
+    this.height = 150;
+    this.width = 50;
   }
   
   draw () {
     c.fillStyle = this.color;
-    c.fillRect(this.position.x, this.position.y, 50, 150);
+    c.fillRect(this.position.x, this.position.y, this.width, this.height);
   }
   
   update () {
     this.draw();
-    this.position.y += 10;
+ 
+    this.position.y += this.velocity.y;
+    // 1024 = canvas.height
+    if (this.position.y + this.height + this.velocity.y >= canvas.height){
+      this.velocity.y = 0;
+    } else {
+      this.velocity.y += gravity;
+      
+    }
   }
 
 }
