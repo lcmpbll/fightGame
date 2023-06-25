91c85a4e49dcf7b353c91c891a84533bda82865d
Liam Cammpbell <liam22campbell@gmail.com>
2023-06-25 15:00:22 -0700

    Update index.js and title

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-  entry: './src/ui-func.js',
+  entry: './src/js/index.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
@@ -15,7 +15,7 @@ module.exports = {
   plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      title: 'Shape Tracker',
+      title: 'fightGame',
       template: './src/index.html',
       inject: 'body'
     })
