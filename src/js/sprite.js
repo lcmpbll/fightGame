9087ae3f9e80c186b9d15c084e1036db4d977fd1
Liam Cammpbell <liam22campbell@gmail.com>
2023-07-01 14:23:13 -0700

    Add background with shop animation

--- src/js/sprite.js
+++ src/js/sprite.js
@@ -1,146 +1,52 @@
 // import $ from 'jquery';
 import { c, canvas } from './index.js';
 
-const gravity = .7;
+
 
 class Sprite {
-  constructor({position, color, velocity, speed, jumpHeight, health = 150, offset}) {
+  constructor({position,  imageSrc, scale = 1, frames = 1, frameHold = 10}) {
     this.position = position;
-    this.color = color;
-    this.velocity = velocity;
-    this.headHeight = 150;
-    this.width = 50;
-    this.isOnGround = false;
-    this.isDucked = false;
-    this.height = 150;
-    this.lastKey;
-    this.speed = speed;
-    this.jumpHeight = jumpHeight;
-    this.attackBox = {
-      position: {
-        x: this.position.x,
-        y: this.position.y
-      },
-      offset,
-      width: 100,
-      height: 50,
-      color: 'yellow'
-    };
-    this.isAttacking = false;
-    this.health = {
-      currentHealth: health,
-      totalHealth: health
-    },
-    this.frontEdge = this.width;
-    this.damage = 10;
-    this.gamesWon = 0;
+    this.height = canvas.height;
+    this.width = canvas.width;
+    this.image = new Image();
+    this.image.src = imageSrc;
+    this.scale = scale;
+    this.frames = frames;
+    this.currentFrame = 0;
+    this.framesElapsed = 0,
+    this.framesHold = frameHold;
+    
+    
+
+  
+  
   }
   
   draw () {
-    c.fillStyle = this.color;
-    c.fillRect(this.position.x, this.position.y, this.width, this.height);
-    
-    // attack box
-    if(this.isAttacking){
-      c.fillStyle = 'yellow';
-      c.fillRect(this.attackBox.position.x, this.attackBox.position.y, this.attackBox.width, this.attackBox.height);
-      
-    }
+    c.drawImage(this.image, this.currentFrame * (this.image.width / this.frames), 0, this.image.width/this.frames , this.image.height, this.position.x, this.position.y, this.image.width/this.frames * this.scale, this.image.height * this.scale);
   }
   
   update () {
     this.draw();
-    this.attackBox.position.x = this.position.x + this.attackBox.offset.x;
-    this.attackBox.position.y = this.position.y;
-    this.position.x += this.velocity.x;
-    this.position.y += this.velocity.y;
-    // 524 = canvas.height
-    if (this.position.y + this.headHeight + this.velocity.y >= 524){
-      this.velocity.y = 0;
-      this.isOnGround = true;
-    } else {
-      this.velocity.y += gravity;
-      this.isOnGround = false;
-      
-      
-    }
-  }
-  
-  jump () {
-
-    if(this.isOnGround === true && this.isDucked === false){
-      this.velocity.y = this.velocity.y - this.jumpHeight;
-
-     
-    }
-  }
-  
-  
-  shortJump () {
-    if(this.isOnGround === true && this.isDucked === false){
-      this.isDucked = true;
-      this.height = this.headHeight * 0.5;
-    }
-  }
-  
-  shortLand () {
-    this.isDucked = false;
-    this.height = this.headHeight;
-  }
-  
-  //currently more of a leg sweep dodge
-  duck () {
-    if(this.isOnGround === true && this.isDucked === false){
-      this.isDucked = true;
-      this.position.y +=this.headHeight * 0.5;
-    }
-  }
-  
-  stand () {
-    this.isDucked = false;
-    this.position.y -= this.headHeight * 0.5;
-  }
-  
-  switch () {
-    if(this.lastKey === 'd' || this.lastKey === 'aR'){
-      
-      this.attackBox.offset.x = 0;
-    } else {
-      this.attackBox.offset.x = -50; 
-    }
-  
-  }
-  
-  
-  attack () {
-    this.isAttacking = true;
-    setTimeout(()=> {
-      this.isAttacking = false;
-    }, 100);
-    // this.attackCheck(target);
-  }
-
-  getCurrentHealthFraction (damage) {
-    this.health.currentHealth -= damage;
-    return this.health.currentHealth / this.health.totalHealth;
-  }
-  
-  recordWin () {
-    this.gamesWon ++;
-  }
-  
-  reset(resetPositions) {
-
-    this.health.currentHealth = this.health.totalHealth;
-    this.position.x = resetPositions.position.x;
-    this.position.y = resetPositions.position.y;
-    // this.attackBox.offset = this.resetPosition.face;
     
-    this.velocity.x = resetPositions.velocity.x;
-    this.velocity.y = resetPositions.velocity.y;
-    // this.switch();
-    // this.update(); 
+    this.framesElapsed ++;
+    if(this.frames > 1 && this.framesElapsed % this.framesHold === 0){
+      if (this.frames - 1  > this.currentFrame){
+        
+        this.currentFrame ++;
+      } else {
+        this.currentFrame = 0;
+      }
+    } 
+  
   }
+  
+
+  
+  
+
+  
+
   
 
 }
