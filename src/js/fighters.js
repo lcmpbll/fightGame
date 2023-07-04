e864908238ca2f0949bc768fae8bc343e8cd7e90
Liam Cammpbell <liam22campbell@gmail.com>
2023-07-04 13:31:14 -0700

    Fix attack box positions

--- src/js/fighters.js
+++ src/js/fighters.js
@@ -1,20 +1,21 @@
 // import $ from 'jquery';
-import { canvas, spriteNames } from './index.js';
+import { c, canvas, spriteNames } from './index.js';
 import Sprite from './sprite.js';
 
 const gravity = .7;
+const groundLevel = 374.4999999999999;
 
 class Fighter extends Sprite {
   
-  constructor({position, color, velocity, speed, jumpHeight, health = 150, offset, imageSrc, scale = 1, frames = 1, frameHold = 10, imgOffset = {x: 0, y: 0}, sprites = {}}) {
+  constructor({position, color, velocity, speed, jumpHeight, health = 150, imageSrc, scale = 1, frames = 1, frameHold = 10, imgOffset = {x: 0, y: 0}, sprites = {}, attackBox = {offset: {}, width: 100, height: 150}}) {
     super({
       position,
       imageSrc,
       scale,
       frames,
       frameHold,
-      imgOffset
-     
+      imgOffset,
+      
     });
     
     this.color = color;
@@ -32,11 +33,12 @@ class Fighter extends Sprite {
         x: this.position.x,
         y: this.position.y
       },
-      offset,
-      width: 100,
-      height: 50,
-      color: 'yellow'
+      offset: attackBox.offset,
+      width: attackBox.width,
+      height: attackBox.height,
+    
     };
+
     this.isAttacking = false;
     this.health = {
       currentHealth: health,
@@ -73,13 +75,16 @@ class Fighter extends Sprite {
     this.draw();
     this.animateFrames();
     this.attackBox.position.x = this.position.x + this.attackBox.offset.x;
-    this.attackBox.position.y = this.position.y;
+    this.attackBox.position.y = this.position.y + this.attackBox.offset.y;
+    // c.fillRect(this.attackBox.position.x, this.attackBox.position.y, this.attackBox.width, this.attackBox.height);
     this.position.x += this.velocity.x;
     this.position.y += this.velocity.y;
     // y movement
+
     if (this.position.y + this.headHeight + this.velocity.y >= canvas.height){
       this.velocity.y = 0;
       this.isOnGround = true;
+      this.position.y = groundLevel;
     } else {
       this.switchSprite(spriteNames.fall);
       this.velocity.y += gravity;
@@ -87,6 +92,7 @@ class Fighter extends Sprite {
       // this.image = this.sprites[jump].imageS
       
     }
+    
     
     if(this.position.x >= 1024){
       this.velocity.x = 0;
@@ -133,23 +139,30 @@ class Fighter extends Sprite {
   }
   
   switch () {
-    if(this.lastKey === 'd' || this.lastKey === 'aR'){
+    // if(this.lastKey === 'd' || this.lastKey === 'aR'){
       
-      this.attackBox.offset.x = 0;
-    } else {
-      this.attackBox.offset.x = -50; 
-    }
+    //   this.attackBox.offset.x *= -1;
+      
+    // } else {
+    //   // this.attackBox.offset.x *= -1; 
+    // }
   
   }
   
+  takeHit(damage){
+    this.health.current -= damage;
+    this.switchSprite(spriteNames.takeHit);
+  }
   
   attack () {
-    this.isAttacking = true;
-    this.switchSprite(spriteNames.attack1);
-    setTimeout(()=> {
-      this.isAttacking = false;
-      
-    }, 1000);
+    if(this.isAttacking === false){
+      this.isAttacking = true;
+      this.switchSprite(spriteNames.attack1);
+      // setTimeout(()=> {
+      //   this.isAttacking = false;
+        
+      // }, 1000);
+    }
     // this.attackCheck(target);
   }
 
@@ -176,57 +189,72 @@ class Fighter extends Sprite {
   }
   
   switchSprite(sprite){
+    
+    let switched = false;
+    if(this.image === this.sprites.attack1.image && this.currentFrames < this.sprites.attack1.frames -1) return;
+    
     switch(sprite) {
     case spriteNames.idle:
       if(this.image !== this.sprites.idle.image){
         
         this.image = this.sprites.idle.image;
         this.frames = this.sprites.idle.frames;
+        switched = true;
       }
       break;
     case spriteNames.run:
       if(this.image !== this.sprites.run.image){
         this.image = this.sprites.run.image;
         this.frames = this.sprites.run.frames;
+        switched = true;
       }
       break;
     case spriteNames.attack1: 
       if(this.image !== this.sprites.attack1.image){
         this.image = this.sprites.attack1.image;
         this.frames = this.sprites.attack1.frames;
+        switched = true;
       }
       break;
     case spriteNames.attack2: 
       if(this.image !== this.sprites.attack2.image){
         this.image = this.sprites.attack2.image;
         this.frames = this.sprites.attack2.frames;
+        switched = true;
       }
       break;
     case spriteNames.jump: 
       if(this.image !== this.sprites.jump.image){
         this.image = this.sprites.jump.image;
         this.frames = this.sprites.jump.frames;
+        switched = true;
       }
       break;
     case spriteNames.fall: 
       if(this.image !== this.sprites.fall.image){
         this.image = this.sprites.fall.image;
         this.frames = this.sprites.fall.frames;
+        switched = true;
       }
       break;
     case spriteNames.takeHit: 
       if(this.image !== this.sprites.takeHit.image){
         this.image = this.sprites.takeHit.image;
         this.frames = this.sprites.takeHit.frames;
+        switched = true;
       }
       break;
     case spriteNames.death: 
       if(this.image !== this.sprites.death.image){
         this.image = this.sprites.death.image;
         this.frames = this.sprites.death.frames;
+        switched = true;
       }
       break;
-      
+    }
+    if(switched === true){
+      this.currentFrame = 0;
+      this.framesElapsed = 0;
     }
   }
   
